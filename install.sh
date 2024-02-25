3f644cf019a843cda1ce6cd93b80050e0987ec0a
diogorodriguees <diogorodrigueslife@gmail.com>
2024-02-25 14:05:04 -0300

    :sparkles: fixing issue on install nvm

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 echo "[Node] Installing nvm and node latest ..." 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
 nvm install node
 npm i -g yarn
 
